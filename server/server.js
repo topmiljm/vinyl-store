87f224459f24ce03deaeac8b37143ef517619cc0
James <james.m.topmiller@gmail.com>
2026-04-18 10:40:15 -0700

    update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,10 @@ app.use("/webhook", express.raw({ type: "application/json" }));
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [
+      "http://localhost:3000",
+      process.env.CLIENT_URL
+    ],
     credentials: true,
   })
 );
